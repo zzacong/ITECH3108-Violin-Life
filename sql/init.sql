f0227f6af73280fb3fa3b26a5f4415d1292222c4
zzacong <ongzz100@gmail.com>
2020-12-26 17:28:33 +1100

    database sql

---
+++ sql/init.sql
@@ -0,0 +1,63 @@
+-- 
+-- ITECH3108 Assignment 1
+-- ZHI ZAO ONG
+-- 30360914
+-- 
+
+-- Create Database
+CREATE DATABASE `itech3108_30360914_a1`;
+USE `itech3108_30360914_a1`;
+
+-- Drop Tables if exists
+DROP TABLE IF EXISTS 
+  `user`,
+  `violin`,
+  `offer`,
+  `message`
+;
+
+-- Create Table
+CREATE TABLE `user` (
+  `id`        SERIAL,
+  `name`      VARCHAR(255) NOT NULL UNIQUE,
+  `email`     VARCHAR(255) NOT NULL,
+  `password`  VARCHAR(255) NOT NULL,
+  `location`  VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `violin` (
+  `id`            SERIAL,
+  `user_id`       BIGINT UNSIGNED,
+  `title`         VARCHAR(255) NOT NULL,
+  `description`   VARCHAR(255),
+  `seeking`       VARCHAR(255),
+  `submitted`     TIMESTAMP DEFAULT NOW(),
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`user_id`) REFERENCES `user`(`id`)
+);
+
+CREATE TABLE `offer` (
+  `id`            SERIAL,
+  `user_id`       BIGINT UNSIGNED,
+  `violin_id`     BIGINT UNSIGNED,
+  `offer`         VARCHAR(255) NOT NULL,
+  `accepted`      TIMESTAMP,
+  `submitted`     TIMESTAMP DEFAULT NOW(),
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`user_id`) REFERENCES `user`(`id`),
+  FOREIGN KEY (`violin_id`) REFERENCES `violin`(`id`)
+);
+
+CREATE TABLE `message` (
+  `id`              SERIAL,
+  `from_user_id`    BIGINT UNSIGNED,
+  `to_user_id`      BIGINT UNSIGNED,
+  `offer_id`        BIGINT UNSIGNED,
+  `sent`            TIMESTAMP DEFAULT NOW(),
+  `text`            VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`from_user_id`) REFERENCES `user`(`id`),
+  FOREIGN KEY (`to_user_id`) REFERENCES `user`(`id`),
+  FOREIGN KEY (`offer_id`) REFERENCES `offer`(`id`)
+);
